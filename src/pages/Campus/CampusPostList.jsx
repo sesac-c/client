9725a7a51b3a52370976bf9e304d8ac250967a65
KIM SOOBIN <xpsxm225@naver.com>
2024-07-06 17:21:45 +0900

    Feat: Carousel 컴포넌트 정의

--- src/pages/Campus/CampusPostList.jsx
+++ src/pages/Campus/CampusPostList.jsx
@@ -2,10 +2,18 @@ import FeedWrapper from "../../components/Feed/FeedWrapper.jsx";
 import ColumnLayoutWrapper from "../../components/common/layout/ColumnLayoutWrapper.jsx";
 import Posts from "../../components/Feed/Posts.jsx";
 import UserSearch from "../../components/Feed/UserSearch.jsx";
+import Carousel from "../../components/Feed/Carousel.jsx";
+import { dummyNoticesData } from "../../assets/mockData/notice.js";
 
 const CampusPostListPage = () => {
     return (
-        <FeedWrapper boardContent={<>boardContent</>}>
+        <FeedWrapper 
+            boardContent={
+            <Carousel
+                items={dummyNoticesData}
+                title='최신 공지'
+            />
+        }>
             <ColumnLayoutWrapper
                 mainArea={<Posts />}
                 rightSide={<UserSearch/>}
