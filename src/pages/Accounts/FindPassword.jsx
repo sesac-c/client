a4030b04a549ad335dc871b2c4913ba685461a02
KIM SOOBIN <xpsxm225@naver.com>
2024-08-14 22:13:17 +0900

    Refactor: FindPasswordPage 리팩토링
    - Page의 컨텐츠 분리 -> FindPasswordContent
    - Page의 훅 분리 -> useFindPasswordState, FIND_PASSWORD_PROCESS_STATUS
    - Page 상수 정리 -> BUTTON_SIZE, BUTTON_TEXT

--- src/pages/Accounts/FindPassword.jsx
+++ src/pages/Accounts/FindPassword.jsx
@@ -1,66 +1,41 @@
-import { useNavigate } from 'react-router-dom';
-import ProcessErrorModal from '../../components/common/ProcessErrorModal.jsx';
-import HeaderedInputText from '../../components/common/UI/HeaderedInputText.jsx';
 import Modal from '../../components/common/UI/Modal.jsx';
-import { useState } from 'react';
 import Button from '../../components/common/UI/Button.jsx';
-import { RESET_PASSWORD_PATH } from '../../constants/routes.js';
-import VerificationCodeField from '../../components/accounts/VerificationCodeField.jsx';
-import { useNavigateHandler } from '../../hooks/useNavigateHandler.js';
+import ProcessErrorModal from '../../components/common/ProcessErrorModal.jsx';
+import FindPasswordContent from '../../components/Accounts/FindPasswordContent.jsx';
+import useFindPasswordState from '../../hooks/Accounts/useFindPasswordState';
+import { useNavigateHandler } from '../../hooks/useNavigateHandler';
+import { FIND_PASSWORD_PROCESS_STATUS } from '../../hooks/Accounts/useFindPasswordState';
+import { LOGIN_PATH } from '../../constants/routes';
+import { PAGE_MODAL } from '../../constants/modal';
+
+const TITLE = '비밀번호 찾기';
+const BUTTON_SIZE = 'large';
+const BUTTON_TEXT = {
+  [FIND_PASSWORD_PROCESS_STATUS.EMAIL]: '인증번호 발송',
+  [FIND_PASSWORD_PROCESS_STATUS.CODE]: '확인'
+};
 
 const FindPasswordPage = () => {
-  const [findPasswordProcessStatus, setFindPasswordProcessStatus] = useState('email');
-  const navigate = useNavigate();
-  const error = false; //TODO: status error로 수정 필요
+  const { state, handleButtonClick } = useFindPasswordState();
 
-  const isSendVerificationCode = findPasswordProcessStatus === 'verificationCode';
-  let button;
-
-  // TODO: 실제 로직 구현 필요, 비밀번호 설정 페이지 만들기
-  if (findPasswordProcessStatus === 'email') {
-    button = (
-      <Button
-        size='large'
-        onClick={() => {
-          setFindPasswordProcessStatus('verificationCode');
-        }}
-      >
-        인증번호 발송
-      </Button>
-    );
-  } else {
-    button = (
-      <Button
-        size='large'
-        onClick={() => {
-          navigate(RESET_PASSWORD_PATH);
-        }}
-      >
-        확인
-      </Button>
-    );
+  if (state.isError) {
+    return <ProcessErrorModal title='비밀번호 찾기 실패' onClose={useNavigateHandler(LOGIN_PATH)} />;
   }
 
-  if (error) {
-    return <ProcessErrorModal buttonTo='login' title='비밀번호 찾기 실패' />;
-  } else {
-    return (
-      <Modal modalType='pagemodal' title='비밀번호 찾기' footer={button} onClose={useNavigateHandler('../login')}>
-        <div className='modal-form'>
-          <HeaderedInputText
-            title='이메일 인증'
-            placeholder='이메일'
-            name='email'
-            type='email'
-            disabled={isSendVerificationCode}
-            inputMessage={isSendVerificationCode && '인증코드가 발송되었습니다!'}
-            inputMessageType={isSendVerificationCode && 'success'}
-          />
-          {isSendVerificationCode && <VerificationCodeField />}
-        </div>
-      </Modal>
-    );
-  }
+  return (
+    <Modal
+      modalType={PAGE_MODAL}
+      title={TITLE}
+      footer={
+        <Button size={BUTTON_SIZE} onClick={handleButtonClick}>
+          {BUTTON_TEXT[state.currentStep]}
+        </Button>
+      }
+      onClose={useNavigateHandler(LOGIN_PATH)}
+    >
+      <FindPasswordContent currentStep={state.currentStep} />
+    </Modal>
+  );
 };
 
 export default FindPasswordPage;
