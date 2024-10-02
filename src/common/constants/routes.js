665e938d4ae0772f162640530dc4f57b9df4099e
rtg <rtg1539@naver.com>
2024-10-02 16:25:58 +0900

    refactor: selectBox 수정 - option을 value와 label로 구분하도록 로직 수정

--- src/common/constants/routes.js
+++ src/common/constants/routes.js
@@ -11,16 +11,27 @@ const KO_GROUP = '그룹';
 export const FEED_SUFFIX = '피드';
 
 export const FEED_SELECT_ITEMS = [
-    `${KO_CAMPUS} ${FEED_SUFFIX}`, `${KO_ALL} ${FEED_SUFFIX}`, `${KO_GROUP} ${FEED_SUFFIX}`
+  {
+    label: `${KO_CAMPUS} ${FEED_SUFFIX}`,
+    value: `/feed/${CAMPUS}/posts`
+  },
+  {
+    label: `${KO_ALL} ${FEED_SUFFIX}`,
+    value: `/feed/${ALL}/posts`
+  },
+  {
+    label: `${KO_GROUP} ${FEED_SUFFIX}`,
+    value: `/feed/${GROUP}/posts`
+  }
 ];
 
 export const ACCOUNTS_PATH = '/accounts';
 export const ACCOUNT_CHILDREN_PATH = {
-    login: 'login',
-    signup: 'signup',
-    findPassword: 'find/password',
-    resetPassword: 'reset/password',
-}
+  login: 'login',
+  signup: 'signup',
+  findPassword: 'find/password',
+  resetPassword: 'reset/password'
+};
 
 export const LOGIN_PATH = `${ACCOUNTS_PATH}/${ACCOUNT_CHILDREN_PATH.login}`;
 export const SIGNUP_PATH = `${ACCOUNTS_PATH}/${ACCOUNT_CHILDREN_PATH.signup}`;
@@ -30,80 +41,78 @@ export const RESET_PASSWORD_PATH = `${ACCOUNTS_PATH}/${ACCOUNT_CHILDREN_PATH.res
 export const USER_PATH = '/';
 export const MANAGER_PATH = '/manager';
 
-export const CAMPUS_PATH = 'feed/campus'
+export const CAMPUS_PATH = 'feed/campus';
 export const CAMPUS_CHILDREN_PATH = {
-    postList: 'posts',
-    noticeList: 'notices',
-    search: 'search-post',
-}
+  postList: 'posts',
+  noticeList: 'notices',
+  search: 'search-post'
+};
 
 export const CAMPUS_POST_LIST_PATH = `/${CAMPUS_PATH}/${CAMPUS_CHILDREN_PATH.postList}`;
 export const CAMPUS_NOTICE_LIST_PATH = `/${CAMPUS_PATH}/${CAMPUS_CHILDREN_PATH.noticeList}`;
 export const CAMPUS_SEARCH_PATH = `/${CAMPUS_PATH}/${CAMPUS_CHILDREN_PATH.search}`;
 
 export const CAMPUS_MENU = [
-    {
-        title: '메인',
-        path: CAMPUS_POST_LIST_PATH
-    },
-    {
-        title: '글쓰기'
-    },
-    {
-        title: '공지',
-        path: CAMPUS_NOTICE_LIST_PATH
-    },
-    {
-        title: '검색',
-        path: CAMPUS_SEARCH_PATH
-    },
-]
+  {
+    title: '메인',
+    path: CAMPUS_POST_LIST_PATH
+  },
+  {
+    title: '글쓰기'
+  },
+  {
+    title: '공지',
+    path: CAMPUS_NOTICE_LIST_PATH
+  },
+  {
+    title: '검색',
+    path: CAMPUS_SEARCH_PATH
+  }
+];
 
-export const ALL_PATH = '/feed/all'
-export const GROUP_PATH = '/feed/group'
-
+export const ALL_PATH = '/feed/all';
+export const GROUP_PATH = '/feed/group';
 
 export const FEED_INFO = {
-    campus: {
-        title: KO_CAMPUS,
-        name: `${KO_CAMPUS} ${FEED_SUFFIX}`,
-        path: CAMPUS_POST_LIST_PATH,
-        menuList: CAMPUS_MENU
-    },
-    total: {
-        title: KO_ALL,
-        name: `${KO_ALL} ${FEED_SUFFIX}`,
-        path: ALL_PATH
-    },
-    group: {
-        title: KO_GROUP,
-        name: `${KO_GROUP} ${FEED_SUFFIX}`,
-        path: GROUP_PATH
-    },
-}
+  campus: {
+    title: KO_CAMPUS,
+    name: `${KO_CAMPUS} ${FEED_SUFFIX}`,
+    path: CAMPUS_POST_LIST_PATH,
+    menuList: CAMPUS_MENU
+  },
+  all: {
+    title: KO_ALL,
+    name: `${KO_ALL} ${FEED_SUFFIX}`,
+    path: ALL_PATH
+  },
+  group: {
+    title: KO_GROUP,
+    name: `${KO_GROUP} ${FEED_SUFFIX}`,
+    path: GROUP_PATH
+  }
+};
 
 // manager
 
 export const MANAGER_PROFILE_PATH = 'profile';
-export const MANAGER_MESSAGE_PATH = 'messages'
+export const MANAGER_MESSAGE_PATH = 'messages';
 export const MANAGER_USER_PATH = 'users';
 export const MANAGER_FEEDS_PATH = 'feeds';
 export const MANAGER_CAMPUS_PATH = 'campuses';
 export const MANAGER_COURSE_PATH = 'courses';
 export const MANAGER_RUNNINGMATE_PATH = 'runningmates';
 
-
 export const MANAGER_USER_CHILDREN_PATH = {
-    campusChangeAccept: 'campus-change-accept'
+  campusChangeAccept: 'campus-change-accept'
 };
 
 export const MANAGER_FEEDS_CHILDREN_PATH = {
-    campusPost: 'campus-posts',
-    allPost: 'all-posts',
-    allNotice: 'all-notices',
-    groupNotice: 'group-notices'
+  campusPost: 'campus-posts',
+  allPost: 'all-posts',
+  allNotice: 'all-notices',
+  groupNotice: 'group-notices'
 };
 
 export const MANAGER_RUNNINGMATE_CHILDREN_PATH = {
-    report: 'activity-reports'
+  report: 'activity-reports'
 };
