9cf16a994907e745192bb424f834a99afbd5d967
rtg <rtg1539@naver.com>
2024-10-10 22:43:53 +0900

    feat: 그룹 피드 과정명 & 유저목록 조회

--- src/user/pages/Group/GroupCourse.jsx
+++ src/user/pages/Group/GroupCourse.jsx
@@ -5,12 +5,13 @@ import UserSearch from '@/user/components/user/UserSearch.jsx';
 import GroupName from '@/user/components/group/GroupName';
 
 import * as React from 'react';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import Notices from '@/user/components/notice/Notices';
 import { fetchNotices } from '@/user/services/api/notices';
 import Restaurant from '@/user/components/group/Restaurant';
 import GroupTabs from '@/user/components/group/GroupTabs';
+import axios from 'axios';
 
 const TABS = [
   {
@@ -37,13 +38,42 @@ const Main = () => {
 };
 
 const GroupCourse = () => {
+  const [course, setCourse] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = useCallback(async courseId => {
+    if (!courseId) return;
+    try {
+      const { data } = await axios.get(`/members/course/${courseId}`);
+      setUsers(data);
+    } catch (error) {
+      console.error('Failed to load users:', error);
+    }
+  }, []);
+
+  const loadCourse = useCallback(async () => {
+    try {
+      const { data } = await axios.get('/user/student/profiles');
+      const { courseId, courseName, campusId, campusName } = data;
+      setCourse({ courseId, courseName, campusId, campusName });
+
+      await loadUsers(courseId);
+    } catch (error) {
+      console.error('Failed to load course:', error);
+    }
+  }, [loadUsers]);
+
+  useEffect(() => {
+    loadCourse();
+  }, [loadCourse]);
+
   return (
-    <FeedWrapper boardContent={<GroupName name={'우'} />}>
+    <FeedWrapper boardContent={<GroupName name={course && `${course.campusName} ${course.courseName}`} />}>
       <ColumnLayoutWrapper
         mainArea={<Main />}
         rightSide={
           <UserSearch
-            users={[]}
+            users={users}
             searchInputPlaceholder='캠퍼스 매니저 검색'
             noSearchContent='일치하는 캠퍼스 매니저가 없습니다.'
             buttonText='쪽지하기'
