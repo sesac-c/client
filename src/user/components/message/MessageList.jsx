3ee395b6ff120db14bcfd1d00dc9053aeb6369c6
rtg <rtg1539@naver.com>
2024-10-17 17:05:13 +0900

    refactor: 쪽지 리스트 수정
    - 긴 내용 ... 처리

--- src/user/components/message/MessageList.jsx
+++ src/user/components/message/MessageList.jsx
@@ -4,60 +4,12 @@ import { formateDateTimeToKorean } from '@/common/utils';
 import ProfileImage from '@/common/components/common/layout/ProfileImage';
 import useMessageStore from '@/user/store/messageStore';
 
-const ReceivedList = ({ messages, newCount }) => {
-  const { readMessage } = useMessageStore();
-
-  return (
-    <>
-      <div className='flex justify-end '>
-        <div className='bg-emerald-300 px-1 text-sm text-emerald-700'>
-          {newCount ? `${newCount}개의 신규 쪽지` : '모두 읽음'}
-        </div>
-      </div>
-      {messages.map(message => (
-        <div key={`message-${message.id}`} className={`my-1 flex px-1 py-3 ${message.isRead ? '' : 'bg-emerald-50'}`}>
-          <div className='user-list__profile-image'>
-            <ProfileImage hasShadow={false} />
-          </div>
-          <div onClick={() => readMessage(message)}>
-            <div className='flex'>
-              <div>{message.sender}</div>
-              <div className='ml-1 content-end text-xs'>{formateDateTimeToKorean(message.createdAt)}</div>
-            </div>
-            <div className='text-xs'>{message.content}</div>
-          </div>
-        </div>
-      ))}
-    </>
-  );
-};
-
-const SentList = ({ messages }) => {
-  const { readMessage } = useMessageStore();
-
-  return (
-    <>
-      {messages.map(message => (
-        <div key={`message-${message.id}`} className={`my-1 flex px-1 py-3`}>
-          <div className='user-list__profile-image'>
-            <ProfileImage hasShadow={false} />
-          </div>
-          <div onClick={() => readMessage(message)}>
-            <div className='flex'>
-              <div>{message.receiver}</div>
-              <div className='ml-1 content-end text-xs'>{formateDateTimeToKorean(message.createdAt)}</div>
-            </div>
-            <div className='text-xs'>{message.content}</div>
-          </div>
-        </div>
-      ))}
-    </>
-  );
-};
-
 const MessageList = ({ messageType }) => {
   const [messages, setMessages] = useState([]);
   const [newCount, setNewCount] = useState(0);
+  const { readMessage } = useMessageStore();
+  const isReceived = () => messageType === 'received';
+
   const load = async () => {
     try {
       console.log(messageType);
@@ -76,11 +28,32 @@ const MessageList = ({ messageType }) => {
 
   return (
     <>
-      {messageType === 'received' ? (
-        <ReceivedList messages={messages} newCount={newCount} />
-      ) : (
-        <SentList messages={messages} />
+      {isReceived() && (
+        <div className='flex justify-end '>
+          <div className='bg-emerald-300 px-1 text-sm text-emerald-700'>
+            {newCount ? `${newCount}개의 신규 쪽지` : '모두 읽음'}
+          </div>
+        </div>
       )}
+
+      {messages.map(message => (
+        <div
+          key={`message-${message.id}`}
+          className={`my-1 flex px-1 py-3 ${isReceived() && !message.isRead ? 'bg-emerald-50' : ''}`}
+        >
+          <div className='user-list__profile-image'>
+            <ProfileImage hasShadow={false} />
+          </div>
+          <div onClick={() => readMessage(message)}>
+            <div className='flex'>
+              {isReceived() ? <div>{message.sender}</div> : <div>{message.receiver}</div>}
+
+              <div className='ml-1 content-end text-xs'>{formateDateTimeToKorean(message.createdAt)}</div>
+            </div>
+            <div className='single-line-content text-xs'>{message.content}</div>
+          </div>
+        </div>
+      ))}
     </>
   );
 };
